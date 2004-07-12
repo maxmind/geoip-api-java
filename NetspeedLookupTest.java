e9a1d69fb7bd410c73fba8be41881cc6720c5ba2
tjmather <tjmather>
2004-07-12 02:06:11 +0000

    formating changes

--- NetspeedLookupTest.java
+++ NetspeedLookupTest.java
@@ -4,13 +4,13 @@ import com.maxmind.geoip.*;
 import java.io.IOException;
 
 /* sample of how to use the GeoIP Java API with GeoIP Netspeed database */
-/* Usage: java NetspeedLookupTest 64.4.4.4 */
+/* Usage: java NetspeedLookupTest 24.24.24.24 */
 
 class NetspeedLookupTest {
     public static void main(String[] args) {
         try {
             LookupService cl = new LookupService("/usr/local/share/GeoIP/GeoIPNetspeed.dat");
-            if (args.length > 0){
+            if (args.length > 0) {
                 int speed = cl.getID(args[0]);
 	        if (speed == cl.GEOIP_UNKNOWN_SPEED){
 		    System.out.println("Unknown");
@@ -21,7 +21,7 @@ class NetspeedLookupTest {
                 } else if (speed == cl.GEOIP_CORPORATE_SPEED) {
 		    System.out.println("Corporate");
                 }
-            }else{
+            } else {
  	        System.out.println("input a ip address\n");
  	    }
 	}
