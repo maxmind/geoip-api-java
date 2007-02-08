c4dc53a8b6ad5b66334029606aa724509bd66a61
tjmather <tjmather>
2007-02-08 05:43:56 +0000

    Added timeZone and regionName classes to lookup timezone and region name

--- CityLookupTest.java
+++ CityLookupTest.java
@@ -10,12 +10,13 @@ class CityLookupTest {
     public static void main(String[] args) {
 	try {
 	    LookupService cl = new LookupService("/usr/local/share/GeoIP/GeoIPCity.dat",
-					LookupService.GEOIP_MEMORY_CACHE | LookupService.GEOIP_CHECK_CACHE );
+					LookupService.GEOIP_MEMORY_CACHE );
             Location l1 = cl.getLocation("213.52.50.8");
             Location l2 = cl.getLocation(args[0]);
 	    System.out.println("countryCode: " + l2.countryCode +
                                "\n countryName: " + l2.countryName +
                                "\n region: " + l2.region +
+                               "\n regionName: " + regionName.regionNameByCode(l2.countryCode, l2.region) +
                                "\n city: " + l2.city +
                                "\n postalCode: " + l2.postalCode +
                                "\n latitude: " + l2.latitude +
@@ -23,7 +24,8 @@ class CityLookupTest {
                                "\n distance: " + l2.distance(l1) +
                                "\n distance: " + l1.distance(l2) + 
  			       "\n dma code: " + l2.dma_code +
- 			       "\n area code: " + l2.area_code);
+ 			       "\n area code: " + l2.area_code +
+                               "\n timezone: " + timeZone.timeZoneByCountryAndRegion(l2.countryCode, l2.region));
 
 	    cl.close();
 	}
