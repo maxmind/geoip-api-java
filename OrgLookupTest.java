6f4329c4ec92b4a936c1b0e99684df1da24f5e86
tjmather <tjmather>
2008-09-11 17:55:35 +0000

    *** empty log message ***

--- OrgLookupTest.java
+++ OrgLookupTest.java
@@ -4,6 +4,7 @@ import com.maxmind.geoip.*;
 import java.io.IOException;
 
 /* sample of how to use the GeoIP Java API with GeoIP Organization and ISP databases */
+/* This example can also be used with the GeoIP Domain and ASNum databases */
 /* Usage: java OrgLookupTest 64.4.4.4 */
 
 class OrgLookupTest {
