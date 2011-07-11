a2eb09827b2166cf74474e559c84a10304b35937
borisz <borisz>
2011-07-11 14:08:53 +0000

    Add more examples ( Boris Zentner )

---
+++ ASNumLookupTestV6.java
@@ -0,0 +1,21 @@
+/* OrgLookupTest.java */
+
+import com.maxmind.geoip.*;
+import java.io.IOException;
+
+/* sample of how to use the GeoIP Java API with GeoIP Organization and ISP databases */
+/* This example can also be used with the GeoIP Domain and ASNum databases */
+/* Usage: java OrgLookupTest 64.4.4.4 */
+
+class ASNumLookupTestV6 {
+    public static void main(String[] args) {
+	try {
+	    LookupService asnl = new LookupService("/usr/local/share/GeoIP/GeoIPASNumv6.dat");
+	    System.out.println("ASNum V6: " + asnl.getOrgV6(args[0]));
+	    asnl.close();
+	}
+	catch (IOException e) {
+	    System.out.println("IO Exception");
+	}
+    }
+}
