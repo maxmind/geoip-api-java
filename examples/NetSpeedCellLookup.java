b02a8f642e5f6d2bdc1ff5ff78b8b6172f8f26de
Boris Zentner <bzentner@maxmind.com>
2014-03-19 20:34:29 +0100

    Clarify comments about NetSpeed and NetSpeedCell usage

--- examples/NetSpeedCellLookup.java
+++ examples/NetSpeedCellLookup.java
@@ -2,7 +2,7 @@ import com.maxmind.geoip.*;
 import java.io.IOException;
 
 /* sample of how to use the GeoIP Java API with GeoIP Organization and ISP databases */
-/* This example can also be used with the GeoIP Domain and ASNum databases */
+/* This example can also be used with the GeoIP Domain, NetSpeedCell and ASNum databases */
 /* Usage: java NetSpeedCellLookup 64.4.4.4 */
 
 class NetSpeedCellLookup {
