6204c686e3e93abfd2678e0e4f7007568b2a2436
nguillaumin <nguillaumin@funnelback.com>
2016-02-06 10:52:51 +1100

    Avoid returning the backing array to prevent modifications.
    Also return a `List<String>` rather than an array for convenience.

--- src/main/java/com/maxmind/geoip/LookupService.java
+++ src/main/java/com/maxmind/geoip/LookupService.java
@@ -29,6 +29,8 @@ import java.nio.ByteBuffer;
 import java.nio.charset.CharacterCodingException;
 import java.nio.charset.Charset;
 import java.nio.charset.CharsetDecoder;
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * Provides a lookup service for information based on an IP address. The
@@ -405,15 +407,15 @@ public class LookupService {
     /**
      * @return The list of all known country names
      */
-    public String[] getAllCountryNames() {
-        return countryName;
+    public List<String> getAllCountryNames() {
+        return Arrays.asList(Arrays.copyOf(countryName, countryName.length));
     }
     
     /**
      * @return The list of all known country codes
      */
-    public String[] getAllCountryCodes() {
-        return countryCode;
+    public List<String> getAllCountryCodes() {
+        return Arrays.asList(Arrays.copyOf(countryCode, countryCode.length));
     }
     
     /**
